33b5c113b1e72c595348cb98bfbad2a2718b8f56
news4vip <news4vip@gmail.com>
2016-05-22 00:35:03 +0900

    ポップアップが消えないバグを修正。たぶん直っている。

--- PanaMap/Scripts/panamap.js
+++ PanaMap/Scripts/panamap.js
@@ -2,10 +2,10 @@
 	var map;
 	var overlay;
 	var select;
+	var popup = {};
 
 	var epsg3857   = new OpenLayers.Projection("EPSG:3857");
 	var epsg4326   = new OpenLayers.Projection("EPSG:4326");
-	var epsg900913 = new OpenLayers.Projection("EPSG:900913");
 
 	function init() {
 		// The overlay layer for our marker, with a simple diamond as symbol
@@ -36,7 +36,7 @@
 		map.events.register("moveend" , map, onMapChange);
 
 		// 中心点を日本にする
-		var lonlat = new OpenLayers.LonLat(center.lon, center.lat).transform(epsg4326,epsg900913)
+		var lonlat = new OpenLayers.LonLat(center.lon, center.lat).transform(epsg4326,epsg3857)
 		map.setCenter(lonlat, center.zoom);
     }
 
@@ -49,7 +49,7 @@
 	    lonLat.lon = Math.round(lonLat.lon*1000000)/1000000;
 
 	    // 表示範囲
-	    var bounds = map.getExtent().transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
+	    var bounds = map.getExtent().transform(epsg3857, epsg4326);
 
 	    // データ取ってくる
 		var url = '/api/map/' + bounds.top + ',' + bounds.left + ',' + bounds.bottom + ',' + bounds.right;
@@ -58,7 +58,7 @@
 			$.each(rows, function(key,val) {
 				// マーカー座標
 				var point = new OpenLayers.Geometry.Point(val.google_latlon.Y, val.google_latlon.X)
-					.transform(epsg4326, epsg900913);
+					.transform(epsg4326, epsg3857);
 				// マーカー生成
 				var feature = new OpenLayers.Feature.Vector(point, {
 			        tooltip: val.address,
@@ -66,13 +66,26 @@
 			    });
 				features.push(feature);
 			});
-			overlay.removeAllFeatures();
+
+			if (popup.feature != null) 
+			{// ポップアップが開いている場合は1個だけ残す
+				var tmp = overlay.features.concat();
+				var pos = tmp.indexOf(popup.feature);
+				tmp.splice(pos,1);
+				overlay.removeFeatures(tmp);
+			} 
+			else 
+			{// ポップアップが開いていない場合は全部消す
+				overlay.removeAllFeatures();
+			}
 			overlay.addFeatures(features);
+
 		});
 	}
 
 	function onPopupClose(evt) {
 	    select.unselect(this.feature);
+	    this.feature = null;
 	}
 	
 	function onFeatureSelect(evt) {
